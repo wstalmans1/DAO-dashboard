c43927f20fdcad27991c5bd9b2c57358fbb18814
Wim Stalmans <wim.stalmans@gmail.com>
2024-07-01 15:27:19 +0200

    2nd commit

--- src/main.tsx
+++ src/main.tsx
@@ -21,8 +21,6 @@ const router = createHashRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-
         <RouterProvider router={router} />
-
   </React.StrictMode>,
 )
